1737510a10a584935551701d038e012fd2e80844
Steve E. <steve.ehrhardt@gmail.com>
2026-04-13 17:00:29 -0400

    fourth commit

--- dog.go
+++ dog.go
@@ -1,12 +1,13 @@
 package dog
 
 import (
+	"fmt"
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
 	fmt.Println("Value of s", s)
-	if strings.TrimSpace(s) == "" { /* is blank */ } {
+	if strings.TrimSpace(s) == "" {
 		s = "I am not Mahala the adult"
 	}
 	fmt.Println("Value of s", s)
